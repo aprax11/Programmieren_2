42f15f7fd981ed76a5a5e75f03f286b16e671f78
aprax11 <72447866+aprax11@users.noreply.github.com>
2021-01-25 18:57:59 +0100

    Add files via upload

--- out/production/demo/com/company/tcpFileExchange/TCPFileExchange.java
+++ out/production/demo/com/company/tcpFileExchange/TCPFileExchange.java
@@ -3,7 +3,18 @@ package tcpFileExchange;
 import java.io.IOException;
 
 public interface TCPFileExchange {
-    void connect2Host(String filename, String hostname, int port) throws IOException;
+    /**
+     * Send a file to host via TCP
+     * @param filename
+     * @param hostname
+     * @param port
+     */
+    void sendFile2Host(String filename, String hostname, int port) throws IOException;
 
-    void acceptConnection(String filename, int port) throws IOException;
+    /**
+     * Receive content from a TCP client and write it in a lokal file
+     * @param filename
+     * @param port to accept connection
+     */
+    void receiveFile(String filename, int port) throws IOException;
 }
